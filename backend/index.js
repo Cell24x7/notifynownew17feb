b4f2306099be59ad3f56785229307b236635284f
Sandeep Yadav <sandeepengineering03@gmail.com>
2026-03-13 12:46:44 +0530

    Fix Developer Webhook Route to bypass Nginx 405 constraint

--- backend/index.js
+++ backend/index.js
@@ -105,8 +105,8 @@ app.use('/api/whatsapp-pinbot', require('./routes/whatsappPinbot'));
 app.use('/api/chatflows', require('./routes/chatflows'));
 app.use('/api/feedbacks', require('./routes/feedbacks'));
 
-// Developer Webhook Endpoint
-app.use('/webhook', require('./routes/developerWebhooks'));
+// Developer Webhook Endpoint (Must have /api/ prefix for Nginx proxy pass)
+app.use('/api/webhook', require('./routes/developerWebhooks'));
 
 /* ==================================
    HEALTH & FRONTEND
